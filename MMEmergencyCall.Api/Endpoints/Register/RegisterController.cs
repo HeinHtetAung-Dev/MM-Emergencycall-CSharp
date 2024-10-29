2916fd838659616bfb643df66f4df10fce2cb6ac
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-10-29 20:58:08 +0630

    feat: add api project

---
+++ MMEmergencyCall.Api/Endpoints/Register/RegisterController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MMEmergencyCall.Domain.Features.Register;
+
+namespace MMEmergencyCall.Api.Endpoints.Register
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegisterController : ControllerBase
+    {
+        private readonly RegisterService _registerService;
+
+        public RegisterController(RegisterService registerService)
+        {
+            _registerService = registerService;
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterRequestModel requestModel)
+        {
+            return Ok(_registerService.Register(requestModel)); 
+        }
+    }
+}
