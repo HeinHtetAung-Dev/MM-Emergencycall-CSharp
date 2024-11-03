d9d01b5ab747e145454003c7cb73a702e38d664f
Lin Thit <linthit2745@gmail.com>
2024-11-03 11:47:08 +0630

    [add] emergency request getall,getbyid,add & update

---
+++ MMEmergencyCall.Domain/Features/EmergencyRequests/EmergencyRequestListResponseModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MMEmergencyCall.Domain.Features.EmergencyRequests;
+
+public class EmergencyRequestListResponseModel
+{
+    public Result<List<EmergencyRequest>> Result { get; set; }
+
+    public EmergencyRequestListResponseModel(Result<List<EmergencyRequest>> result)
+    {
+        Result = result;
+    }
+}
---
+++ MMEmergencyCall.Domain/Features/EmergencyRequests/EmergencyRequestRequestModel.cs
@@ -0,0 +1,20 @@
+﻿namespace MMEmergencyCall.Domain.Features.EmergencyRequests;
+
+public class EmergencyRequestRequestModel
+{
+    public int UserId { get; set; }
+
+    public int ServiceId { get; set; }
+
+    public int? ProviderId { get; set; }
+
+    public DateTime RequestTime { get; set; }
+
+    public string Status { get; set; } = null!;
+
+    public DateTime? ResponseTime { get; set; }
+
+    public string? Notes { get; set; }
+
+    public string? TownshipCode { get; set; }
+}
