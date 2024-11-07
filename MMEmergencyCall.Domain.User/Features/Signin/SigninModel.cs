b25dc34a5ba19ede16ce2fea2599731fc6384345
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-11-07 23:45:10 +0630

    fix: code changes

---
+++ MMEmergencyCall.Domain.User/Features/Signin/SigninModel.cs
@@ -0,0 +1,14 @@
+﻿namespace MMEmergencyCall.Domain.Client.Features.Signin;
+
+public class SigninModel
+{
+    public int UserId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public DateTime? SessionExpiredTime { get; set; }
+
+    public string Token { get; set; } = null!;
+}
