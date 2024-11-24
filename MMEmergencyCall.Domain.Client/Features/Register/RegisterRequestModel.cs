b871ec8f67bdda0cee617c18f10ca77eb622579c
Su Sandar Lin <susandarlin.2016@gmail.com>
2024-11-24 23:43:31 +0700

    fix: add email, password and role in client register

--- MMEmergencyCall.Domain.Client/Features/Register/RegisterRequestModel.cs
+++ MMEmergencyCall.Domain.Client/Features/Register/RegisterRequestModel.cs
@@ -3,7 +3,9 @@
 public class RegisterRequestModel
 {
     public string Name { get; set; } = null!;
+    public string Email { get; set; } = null!;
     public string PhoneNumber { get; set; } = null!;
+    public string Password { get; set; } = null!;
     public string Address { get; set; } = null!;
     public string TownshipCode { get; set; } = null!;
     public string? EmergencyType { get; set; }
