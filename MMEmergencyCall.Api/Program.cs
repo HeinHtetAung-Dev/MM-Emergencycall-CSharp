a77992f8129c5e7337eeb680b1161c487d5acad2
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-12-19 12:25:08 +0630

    fix: add swagger ui in prod

--- MMEmergencyCall.Api/Program.cs
+++ MMEmergencyCall.Api/Program.cs
@@ -40,11 +40,14 @@ builder.AddProfile();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
