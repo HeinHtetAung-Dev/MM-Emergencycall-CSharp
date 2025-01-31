917e8edf879da14b4232264c093d291841648adb
Aung Moe Oo <aungmoeoo.dev@gmail.com>
2025-01-31 11:08:41 +0630

    fix: split Client EmergencyRequest features

---
+++ MMEmergencyCall.Domain.Client/Features/EmergencyRequestHistory/EmergencyRequestHistoryResponseModel.cs
@@ -0,0 +1,38 @@
+﻿using MMEmergencyCall.Domain.Client.Features.EmergencyRequests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMEmergencyCall.Domain.Client.Features.EmergencyRequestHistory;
+
+public class EmergencyRequestHistoryPaginationResponseModel
+{
+	public int PageNo { get; set; }
+	public int PageSize { get; set; }
+	public int PageCount { get; set; }
+	public bool IsEndOfPage => PageNo == PageCount;
+	public List<EmergencyRequestHistoryResponseModel> Data { get; set; }
+}
+
+public class EmergencyRequestHistoryResponseModel
+{
+	public int RequestId { get; set; }
+
+	public int UserId { get; set; }
+
+	public int ServiceId { get; set; }
+
+	public int? ProviderId { get; set; }
+
+	public DateTime RequestTime { get; set; }
+
+	public string Status { get; set; } = null!;
+
+	public DateTime? ResponseTime { get; set; }
+
+	public string? Notes { get; set; }
+
+	public string? TownshipCode { get; set; }
+}
---
+++ MMEmergencyCall.Domain.Client/Features/SubmitEmergencyRequest/SubmitEmergencyRequestResponseModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMEmergencyCall.Domain.Client.Features.SubmitEmergencyRequest;
+
+public class SubmitEmergencyRequestResponseModel
+{
+	public int RequestId { get; set; }
+
+	public int UserId { get; set; }
+
+	public int ServiceId { get; set; }
+
+	public int? ProviderId { get; set; }
+
+	public DateTime RequestTime { get; set; }
+
+	public string Status { get; set; } = null!;
+
+	public DateTime? ResponseTime { get; set; }
+
+	public string? Notes { get; set; }
+
+	public string? TownshipCode { get; set; }
+}
